5ed6a8b9955b050fd7f72b29a7fb1263623bff35
Jamie Phillips <phillipsj@users.noreply.github.com>
2017-10-24 08:46:40 -0400

    Initial project structure.

---
+++ src/DotnetCake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DotnetCake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Options
+    {
+        public string Script { get; set; }
+        public string Target { get; set; }
+        public string Configuration { get; set; }
+        public string Verbosity { get; set; }
+        public bool SkipToolPackageRestore { get; set; }
+        public string ScriptArgs { get; set; }
+    }
+}
